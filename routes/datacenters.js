a55cc22232c239c030320c3ddc80c75a94810f51
James Zhan <zhiqiangzhan@gmail.com>
2013-04-22 09:21:53 +0800

    Refactor the json directory.

--- routes/datacenters.js
+++ routes/datacenters.js
@@ -1,16 +1,11 @@
-exports.top_hosts = function(req, res){
-  res.set("ContentType", "application/json");
-  res.sendfile(__dirname + '/json/dc_top_hosts.json');  
-}
-
 exports.show = function(req, res){
   res.set("ContentType", "application/json");
-  res.sendfile(__dirname + '/json/dc/0' + req.params.id + '.json');  
+  res.sendfile(__dirname + '/json/dcs/0' + req.params.id + '.json');  
 }
 
 exports.events = function(req, res){
   res.set("ContentType", "application/json");
-  res.sendfile(__dirname + '/json/dc/0' + req.params.id + 'events.json');
+  res.sendfile(__dirname + '/json/dcs/0' + req.params.id + '/events.json');
 }
 
 
@@ -18,12 +13,12 @@ exports.top_hosts = function(req, res){
   res.set("ContentType", "application/json");
   //For actual Logic, you should give the top hosts according to current datacenter id {params.id}
   var rand = Math.ceil(Math.random() * 5)
-  res.sendfile(__dirname + '/json/dc_top_hosts/0' + rand + '.json');  
+  res.sendfile(__dirname + '/json/dcs/top_hosts/0' + rand + '.json');  
 }
 
 exports.top_vms = function(req, res){
   res.set("ContentType", "application/json");
   //For actual Logic, you should give the top vms according to current datacenter id {params.id}
   var rand = Math.ceil(Math.random() * 5)
-  res.sendfile(__dirname + '/json/dc_top_vms/0' + rand + '.json');  
+  res.sendfile(__dirname + '/json/dcs/top_vms/0' + rand + '.json');  
 }
