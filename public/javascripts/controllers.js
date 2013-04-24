0b6ce9fe0e7eb6d323b06472e2fa1e486a65cbe6
James Zhan <zhiqiangzhan@gmail.com>
2013-04-24 15:53:59 +0800

    Add Pagination functionalities.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -36,13 +36,14 @@ function ClusterCtrl($scope, $routeParams, $dialog, Cluster, currentCluster) {
   });
 }
 
-
 function HostCtrl($scope, $routeParams, Host) {
   Host.get({id: $routeParams.id}, function(host){
     $scope.host = host;
     $scope.vms = $scope.host.virtual_machines;
   });
 }
+
+
 
 function VMCtrl($scope, $routeParams, VMService) {
   $scope.selected = {};
@@ -53,8 +54,85 @@ function VMCtrl($scope, $routeParams, VMService) {
   });
 }
 
+function HostMgmtCtrl($scope){  
+  $scope.current_page = 0;
+  $scope.page_size = 10;
+  
+  $scope.$watch('current_page', function(current, old){    
+    if(current == 0){
+      $scope.prev_class = 'disabled';
+    }else{
+      $scope.next_class = 'active';
+    }
+    if(current == $scope.page_count - 1){
+      $scope.next_class = 'disabled';
+    }else{
+      $scope.next_class = 'active';
+    }
+  });
+  
+  $scope.page_count = function(){
+    if($scope.hosts){
+      return Math.ceil($scope.hosts.length/$scope.page_size);
+    }else{
+      return 0;
+    }
+  };
+  
+  $scope.prev = function(){
+    if($scope.current_page > 0){
+      $scope.current_page -= 1;
+    }
+  };
+  $scope.page = function(i){
+    $scope.current_page = i;
+  };
+  $scope.next = function(){
+    if($scope.current_page < $scope.page_count() - 1){
+      $scope.current_page += 1;
+    }
+  };  
+}
+
 function VMMgmtCtrl($scope){
   $scope.selected || ($scope.selected = {});
+  $scope.current_page = 0;
+  $scope.page_size = 10;
+  
+  $scope.$watch('current_page', function(current, old){    
+    if(current == 0){
+      $scope.prev_class = 'disabled';
+    }else{
+      $scope.next_class = 'active';
+    }
+    if(current == $scope.page_count - 1){
+      $scope.next_class = 'disabled';
+    }else{
+      $scope.next_class = 'active';
+    }
+  });
+  
+  $scope.page_count = function(){
+    if($scope.vms){
+      return Math.ceil($scope.vms.length / $scope.page_size);
+    }else{
+      return 0;
+    }
+  };
+  
+  $scope.prev = function(){
+    if($scope.current_page > 0){
+      $scope.current_page -= 1;
+    }
+  };
+  $scope.page = function(i){
+    $scope.current_page = i;
+  };
+  $scope.next = function(){
+    if($scope.current_page < $scope.vms.length / $scope.page_size - 1){
+      $scope.current_page += 1;
+    }
+  };
 }
 
 function VMWorkflowCtrl($scope, dialog, currentCluster, selectedVM) {
